29f728ca7e7da6fb6ee8cc44eb15b78c7b9221d2
praveen-vaska <praveen.vaska@sasi.ac.in>
2025-01-22 14:36:25 +0530

    update file for docker

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ res.json({ inr, usd});
 res.status(500).json({ error: e });
 }
 });
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
